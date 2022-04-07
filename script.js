5cf64cc96e5d147eb988e3ea48bad38c47c1fbf5
Hannah Callison <hannahcallison1007@gmail.com>
2022-04-06 19:55:24 -0700

    Cleaning up js code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Global Variables //
 var startBtn = document.getElementById("start")
 var nextBtn = document.getElementById("next")
 var result = document.getElementById("result")
@@ -12,11 +13,14 @@ var hideOptions = document.querySelectorAll('.ansBtn')
 var pageSelect = document.querySelector("section")
 var highScoreTotal = 0
 var secondsLeft = 5;
+var nameInput = document.getElementById("nameInput")
+var savedScores = document.getElementById('savedScores')
+var submitBtn = document.getElementById('submit')
 
 highScore.addEventListener('click', displayHighScores)
 startBtn.addEventListener('click', startQuiz)
 
-
+// Timer Start //
 function setTime() {
     var secondsLeft = 10;
     // Sets interval in variable
@@ -34,18 +38,21 @@ function setTime() {
     }, 1000);
 }
 
+// Enables answer buttons//
 function enableBtn() {
     hideOptions.forEach(function (hideOption) {
         hideOption.removeAttribute("disabled")
     })
 }
 
+// Disables answer buttons//
 function disableBtn() {
     hideOptions.forEach(function (hideOption) {
         hideOption.setAttribute("disabled", true)
     })
 }
 
+// Starts quiz by showing block with question and answer choices for Question 1//
 function startQuiz() {
     startBtn.style.display = "none"
     nextBtn.style.display = "block"
@@ -54,6 +61,7 @@ function startQuiz() {
     displayQuestion1()
 }
 
+// Starts timer for quiz and displays question and choices for question 1, answer buttons are disabled after answer choice or timer runs out. Next btn triggers next question//
 function displayQuestion1() {
     setTime();
     quizQ.textContent = "What does 'www' stand for in a website browser?"
@@ -81,6 +89,7 @@ function displayQuestion1() {
     nextBtn.addEventListener('click', displayQuestion2)
 }
 
+// Displays question and choices for question 2, answer buttons are disabled after answer choice or timer runs out. Next btn triggers next question//
 function displayQuestion2() {
     enableBtn()
     quizQ.textContent = "What is the name of the largest ocean on Earth?"
@@ -108,6 +117,7 @@ function displayQuestion2() {
     nextBtn.addEventListener('click', displayQuestion3)
 }
 
+// Displays question and choices for question 3, answer buttons are disabled after answer choice or timer runs out. Next btn triggers next question//
 function displayQuestion3() {
     enableBtn()
     quizQ.textContent = "How many hearts does an octopus have?"
@@ -135,6 +145,7 @@ function displayQuestion3() {
     nextBtn.addEventListener('click', displayQuestion4)
 }
 
+// Displays question and choices for question 4, answer buttons are disabled after answer choice or timer runs out. Next btn triggers next question//
 function displayQuestion4() {
     enableBtn()
     quizQ.textContent = "What is the hottest planet in the solar  system?"
@@ -162,6 +173,7 @@ function displayQuestion4() {
     nextBtn.addEventListener('click', displayQuestion5)
 }
 
+// Displays question and choices for question 5, answer buttons are disabled after answer choice or timer runs out. Next btn triggers highscore list//
 function displayQuestion5() {
     enableBtn()
     quizQ.textContent = "Sodium bicarbonate is a staple in most kitchens. What is its other name?"
@@ -189,27 +201,7 @@ function displayQuestion5() {
     nextBtn.addEventListener('click', displayHighScores)
 }
 
-// var startBtn = document.getElementById("start")
-// var nextBtn = document.getElementById("next")
-// var result = document.getElementById("result")
-// var highScore = document.getElementById("highScores")
-// var timer = document.getElementById('timer')
-// var quizQ = document.getElementById("quizQ")
-// var ansA = document.getElementById('answerBtn1')
-// var ansB = document.getElementById('answerBtn2')
-// var ansC = document.getElementById('answerBtn3')
-// var ansD = document.getElementById('answerBtn4')
-// var hideOptions = document.querySelectorAll('.ansBtn')
-// var pageSelect = document.querySelector("section")
-// var highScoreTotal = 0
-// var secondsLeft = 5;
-
-highScore.addEventListener('click', displayHighScores)
-startBtn.addEventListener('click', startQuiz)
-var nameInput = document.getElementById("nameInput")
-var savedScores = document.getElementById('savedScores')
-var submitBtn = document.getElementById('submit')
-
+// Highscore list function disappears quiz content and displays highscore list and highscore list input//
 function displayHighScores() {
     pageSelect.style.display = "none"
     document.getElementById('highScoreBoard').style.display = "block";
@@ -218,6 +210,7 @@ function displayHighScores() {
 
 var scores = [];
 
+// Renders user input and adds to highscore list//
 function renderScores() {
   savedScores.innerHTML = "";
 
@@ -232,6 +225,7 @@ function renderScores() {
   }
 }
 
+// Adds previously saved scores to highscore list//
 function init() {
   var storedScores = JSON.parse(localStorage.getItem("scores"));
 
@@ -242,10 +236,12 @@ function init() {
   renderScores();
 }
 
+// Stores name and score input//
 function storeScore() {
   localStorage.setItem("scores", JSON.stringify(scores));
 }
 
+// Submit button triggers adding name input and pushes score and name to  local storage//
 submitBtn.addEventListener("click", function(event) {
   event.preventDefault();
 
@@ -263,3 +259,4 @@ submitBtn.addEventListener("click", function(event) {
 });
 
 init()
+
